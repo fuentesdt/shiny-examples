3fe8479ec988b8c82c443b6cd9c1b04590a82ca4
David Fuentes <fuentesdt@gmail.com>
2021-03-13 18:24:01 -0600

    WIP:

--- 030-basic-datatable/server.R
+++ 030-basic-datatable/server.R
@@ -2,21 +2,17 @@
 # the 'mpg' dataset.
 library(ggplot2)
 
-function(input, output) {
+function(input, output,session) {
 
+  
   # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
-    data <- mpg
-    if (input$man != "All") {
-      data <- data[data$manufacturer == input$man,]
-    }
-    if (input$cyl != "All") {
-      data <- data[data$cyl == input$cyl,]
-    }
-    if (input$trans != "All") {
-      data <- data[data$trans == input$trans,]
-    }
+    data <- read.csv("./trainingdata.csv", header = TRUE)
     data
   }))
 
+  # change HRPID selection box if row clicked
+  system('echo vglrun itksnap -g /rsrch1/ip/egates1/HistoricalRadPath/ProcessedData/Imaging/HRP0393/HRP0393_PREOP_T1.nii.gz',wait = F)
+  observeEvent(input$table1_rows_selected, observeEvent(input$table1_rows_selected, system('echo vglrun itksnap -g /rsrch1/ip/egates1/HistoricalRadPath/ProcessedData/Imaging/HRP0393/HRP0393_PREOP_T1.nii.gz',wait = F))
+  )
 }
