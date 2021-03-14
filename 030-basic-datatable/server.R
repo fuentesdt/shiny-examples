1b91c63700f2ca9686b7d606de7e25d22bae85d4
David Fuentes <fuentesdt@gmail.com>
2021-03-13 21:34:03 -0600

    ENH: working

--- 030-basic-datatable/server.R
+++ 030-basic-datatable/server.R
@@ -8,11 +8,9 @@ function(input, output,session) {
   # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
     data <- read.csv("./trainingdata.csv", header = TRUE)
-    data
   }))
 
   # change HRPID selection box if row clicked
-  system('echo vglrun itksnap -g /rsrch1/ip/egates1/HistoricalRadPath/ProcessedData/Imaging/HRP0393/HRP0393_PREOP_T1.nii.gz',wait = F)
-  observeEvent(input$table1_rows_selected, observeEvent(input$table1_rows_selected, system('echo vglrun itksnap -g /rsrch1/ip/egates1/HistoricalRadPath/ProcessedData/Imaging/HRP0393/HRP0393_PREOP_T1.nii.gz',wait = F))
-  )
+  observeEvent(input$table_rows_selected, system(paste('echo vglrun itksnap -g /rsrch1/ip/egates1/HistoricalRadPath/ProcessedData/Imaging/HRP0393/HRP0393_PREOP_T1.nii.gz',data$uid[input$table_rows_selected]),wait = T))
+
 }
